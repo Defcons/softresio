a2315129a1ba61c67a6216b4d536bab2b74647a2
Casper V. Kristensen <casper@vkristensen.dk>
2026-02-12 01:08:06 +0100

    refactor sr

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,28 +2,14 @@ import { Hono } from "hono"
 import { serveStatic, upgradeWebSocket } from "hono/deno"
 import * as jwt from "hono/jwt"
 import { randomUUID } from "node:crypto"
-import * as z from "zod"
 import type {
-  Attendee,
   Character,
-  CreateSrRequest,
-  CreateSrResponse,
-  DeleteSrRequest,
-  DeleteSrResponse,
   GetCharactersResponse,
   GetInstancesResponse,
-  GetSrPlusResponse,
-  Guild,
   InfoResponse,
   Raid,
   SignOutResponse,
-  SoftReserve,
-  SrPlus,
-  SrPlusManualChangeRequest,
-  SrPlusManualChangeResponse,
-  User,
 } from "../shared/types.ts"
-import { diff, removeOne } from "../shared/utils.ts"
 import {
   DISCORD_API_ENDPOINT,
   DISCORD_CLIENT_ID,
@@ -32,23 +18,16 @@ import {
   DISCORD_REDIRECT_URI,
   JWT_SECRET,
 } from "./config.ts"
-import { beginWithTimeout, sql } from "./database.ts"
+import { sql } from "./database.ts"
 import guildRoutes from "./guild.ts"
-import raidRoutes, { getRecentRaids } from "./raid.ts"
+import srRoutes from "./sr.ts"
 import { instances } from "./instances.ts"
-import {
-  characterSchema,
-  raidIdSchema,
-  userSchema,
-  uuidSchema,
-} from "./schemas.ts"
+import raidRoutes, { getRecentRaids } from "./raid.ts"
 import { getOrCreateUser, setAuthCookie } from "./utils.ts"
 
 await sql.listen("raid_updated", async (raidId) => {
   if (raidId in clients) {
-    console.log(
-      `raidId: ${raidId}, client: ${clients[raidId].length}`,
-    )
+    console.log(`raidId: ${raidId}, client: ${clients[raidId].length}`)
 
     const [{ raid }] = await sql<
       { raid: Raid }[]
@@ -68,343 +47,11 @@ const app = new Hono()
 
 app.route("/", guildRoutes)
 app.route("/", raidRoutes)
+app.route("/", srRoutes)
 
 app.get("/api/instances", async (c) => {
   const user = await getOrCreateUser(c)
   const response: GetInstancesResponse = { data: instances, user }
-  return c.json(response)
-})
-
-app.post("/api/sr/create", async (c) => {
-  const user = await getOrCreateUser(c)
-  const request = z.object({
-    raidId: raidIdSchema,
-    character: characterSchema,
-    selectedItemIds: z.array(z.number()),
-  }).safeParse(await c.req.json())
-
-  if (!request.data) {
-    const response: CreateSrResponse = {
-      error: {
-        message: "Invalid request",
-        issues: request.error.issues,
-      },
-      user,
-    }
-    return c.json(response, 400)
-  }
-
-  const { raidId, character, selectedItemIds }: CreateSrRequest = request.data
-
-  const softReserves: SoftReserve[] = selectedItemIds.map((
-    itemId,
-  ) => ({ itemId, srPlus: null, comment: null }))
-  const attendee: Attendee = {
-    character,
-    softReserves,
-    user,
-  }
-  const response: CreateSrResponse = await beginWithTimeout(async (tx) => {
-    const [result] = await tx<
-      { raid: Raid }[]
-    >`select raid from raids where raid @> ${{
-      id: raidId,
-    } as never} for update;`
-    const raid = result?.raid
-    if (!raid) return { user, error: { message: "Raid not found" } }
-    if (raid.locked) return { user, error: { message: "Raid is locked" } }
-
-    // need to delete all and add all new
-    const oldNameCharacter = raid.attendees.find((attendee) =>
-      attendee.character.name != character.name &&
-      attendee.user.userId == user.userId
-    )
-
-    // need to diff
-    const sameCharacter = raid.attendees.find((attendee) =>
-      attendee.character.name == character.name
-    )
-
-    let changes: { itemId: number; character: Character; remove: boolean }[] =
-      []
-    if (oldNameCharacter) {
-      // remove all
-      changes = changes.concat(oldNameCharacter.softReserves.map((sr) => ({
-        itemId: sr.itemId,
-        character: oldNameCharacter.character,
-        remove: true,
-      })))
-    }
-    if (sameCharacter) {
-      // log diff
-      const { added, removed } = diff(
-        sameCharacter.softReserves.map((sr) => sr.itemId),
-        attendee.softReserves.map((sr) => sr.itemId),
-      )
-      changes = changes.concat(removed.map((itemId) => ({
-        itemId: itemId,
-        character: sameCharacter.character,
-        remove: true,
-      })))
-      changes = changes.concat(added.map((itemId) => ({
-        itemId: itemId,
-        character: sameCharacter.character,
-        remove: false,
-      })))
-    } else {
-      // add all
-      changes = changes.concat(attendee.softReserves.map((sr) => ({
-        itemId: sr.itemId,
-        character: attendee.character,
-        remove: false,
-      })))
-    }
-
-    for (const { itemId, character, remove } of changes) {
-      raid.activityLog.push(
-        {
-          byUser: user,
-          type: "SrChanged",
-          time: (new Date((new Date()).getTime() + (remove ? 0 : 10)))
-            .toISOString(),
-          change: remove ? "deleted" : "created",
-          character,
-          itemId,
-        },
-      )
-    }
-
-    raid.attendees = raid.attendees.filter((attendee) =>
-      attendee.character.name !== character.name &&
-      attendee.user.userId !== user.userId
-    )
-    raid.attendees = [...raid.attendees, attendee]
-    await tx`update raids set ${sql({ raid: raid } as never)} where raid @> ${{
-      id: raidId,
-    } as never}`
-    return { user, data: raid }
-  })
-  return c.json(response)
-})
-
-app.post("/api/sr/delete", async (c) => {
-  const user = await getOrCreateUser(c)
-
-  const requestRaw = z.object({
-    raidId: z.string().length(5),
-    user: userSchema,
-    itemId: z.number(),
-  }).safeParse(await c.req.json())
-
-  if (!requestRaw.data) {
-    const response: DeleteSrResponse = {
-      error: {
-        message: "Invalid request",
-        issues: requestRaw.error.issues,
-      },
-      user,
-    }
-    return c.json(response, 400)
-  }
-  const request: DeleteSrRequest = requestRaw.data
-
-  const response: DeleteSrResponse = await beginWithTimeout(async (tx) => {
-    const [result] = await tx<
-      { raid: Raid }[]
-    >`select raid from raids where raid @> ${{
-      id: request.raidId,
-    } as never} for update;`
-    const raid = result?.raid
-    if (!raid) return { user, error: { message: "Raid not found" } }
-
-    if (
-      raid.admins.some((u) => u.userId == user.userId) ||
-      (request.user.userId == user.userId && !raid.locked)
-    ) {
-      raid.attendees = raid.attendees.map((attendee) => ({
-        user: attendee.user,
-        character: attendee.character,
-        softReserves: removeOne(
-          (softReserve) =>
-            attendee.user.userId == request.user.userId &&
-            softReserve.itemId == request.itemId,
-          attendee.softReserves,
-        ),
-      })).filter(
-        (attendee) => (attendee.softReserves.length > 0),
-      )
-      raid.activityLog.push(
-        {
-          byUser: user,
-          type: "SrChanged",
-          time: (new Date()).toISOString(),
-          change: "deleted",
-          character: raid.attendees.find((a) =>
-            a.user.userId == request.user.userId
-          )?.character,
-          itemId: request.itemId,
-        },
-      )
-      await tx`update raids set ${
-        sql({ raid: raid } as never)
-      } where raid @> ${{
-        id: request.raidId,
-      } as never}`
-      return { user, data: raid }
-    } else {
-      return {
-        user,
-        error: { message: "You are not allowed to delete that SR" },
-      }
-    }
-  })
-  return c.json(response)
-})
-
-app.post("/api/srplus", async (c) => {
-  const user = await getOrCreateUser(c)
-  const request = z.object({
-    guildId: uuidSchema,
-    characterName: z.string().max(12).min(1),
-    itemId: z.number(),
-    value: z.number().min(0).max(1000),
-  }).safeParse(await c.req.json())
-
-  if (!request.data) {
-    const response: SrPlusManualChangeResponse = {
-      error: {
-        message: "Invalid request",
-        issues: request.error.issues,
-      },
-      user,
-    }
-    return c.json(response, 400)
-  }
-
-  const {
-    guildId,
-    characterName,
-    itemId,
-    value,
-  }: SrPlusManualChangeRequest = request.data
-
-  const response: SrPlusManualChangeResponse = await beginWithTimeout(
-    async (tx) => {
-      const [result] = await sql<
-        { guild: Guild }[]
-      >`select guild 
-          from guilds 
-          where 
-            guild @> ${{
-        admins: [{ userId: user.userId }],
-        id: guildId,
-      } as never} for update;`
-      if (!result?.guild) {
-        return ({
-          error: {
-            message: "You are not an admin of a guild with that id",
-          },
-          user,
-        })
-      }
-      const guild = result.guild
-      guild.srPlus.push({
-        type: "manual",
-        time: (new Date()).toISOString(),
-        characterName,
-        itemId,
-        value,
-      })
-      await tx`insert into guilds ${
-        sql({ guild } as never)
-      } on conflict ((guild->>'id')) do update set guild = EXCLUDED.guild;`
-      return { user, data: guild }
-    },
-  )
-  return c.json(response)
-})
-
-app.get("/api/srplus/:raidId", async (c) => {
-  const user = await getOrCreateUser(c)
-  const request = z.string().length(5).safeParse(c.req.param("raidId"))
-  if (!request.data) {
-    const response: GetSrPlusResponse = {
-      error: { message: "Missing raidId from request" },
-      user,
-    }
-    return c.json(response)
-  }
-  const raidId = request.data
-  const [raidResult] = await sql<
-    { raid: Raid }[]
-  >`select raid from raids where raid @> ${{
-    id: raidId,
-  } as never};`
-  const raid = raidResult?.raid
-  if (!raid) {
-    return c.json({ error: { message: "Raid not found" } }, 404)
-  }
-  if (!raid.guildId) {
-    return c.json({ error: { message: "Raid has no guild" } }, 400)
-  }
-  const [guildResult] = await sql<
-    { guild: Guild }[]
-  >`select guild 
-      from guilds 
-      where 
-        guild @> ${{
-    id: raid.guildId,
-  } as never};`
-  if (!guildResult?.guild) {
-    return c.json({
-      error: {
-        message: "Guild not found",
-      },
-      user,
-    })
-  }
-  const guild = guildResult.guild
-  const srPlus: SrPlus[] = []
-  for (const attendee of raid.attendees) {
-    for (const softReserve of attendee.softReserves) {
-      const raids = await sql<
-        { id: string; time: string }[]
-      >`select raid->'id' as id, raid->'time' as time from raids where raid @> ${{
-        guildId: raid.guildId,
-        attendees: [
-          {
-            character: {
-              name: attendee.character.name,
-            },
-            softReserves: [
-              { itemId: softReserve.itemId },
-            ],
-          },
-        ],
-      } as never} and raid->>'time' < ${raid.time};`
-      for (const raid of raids) {
-        srPlus.push({
-          type: "raid",
-          raidId: raid.id,
-          time: raid.time,
-          characterName: attendee.character.name,
-          itemId: softReserve.itemId,
-        })
-      }
-      for (const srPlusChange of guild.srPlus) {
-        if (
-          srPlusChange.itemId == softReserve.itemId &&
-          srPlusChange.characterName == attendee.character.name
-        ) {
-          srPlus.push(srPlusChange)
-        }
-      }
-    }
-  }
-  const response: GetSrPlusResponse = {
-    user,
-    data: srPlus,
-  }
   return c.json(response)
 })
 
@@ -422,7 +69,7 @@ app.get(
     return {
       onOpen(event, ws) {
         const raidId = c.req.param("raidId")
-        clients[raidId] = [{ ws, id }, ...clients[raidId] || []]
+        clients[raidId] = [{ ws, id }, ...(clients[raidId] || [])]
       },
       onClose(event, ws) {
         const raidId = c.req.param("raidId")
@@ -437,13 +84,15 @@ app.get("/api/characters", async (c) => {
   const raids = await getRecentRaids(user, 20)
   const distinctCharacters: Character[] = []
   const characters = raids.flatMap((raid) =>
-    raid.attendees.filter((attendee) => attendee.user.userId == user.userId)
+    raid.attendees
+      .filter((attendee) => attendee.user.userId == user.userId)
       .map((attendee) => attendee.character)
   )
   for (const char of characters) {
     if (
-      !distinctCharacters.some((c) =>
-        c.name == char.name && c.class == char.class && c.spec == char.spec
+      !distinctCharacters.some(
+        (c) =>
+          c.name == char.name && c.class == char.class && c.spec == char.spec,
       )
     ) {
       distinctCharacters.push(char)
@@ -480,22 +129,25 @@ app.get("/api/discord", async (c) => {
     return c.json({ error: { message: "Discord login is not enabled" } })
   }
   const accessRequest = new URLSearchParams({
-    "grant_type": "authorization_code",
-    "code": c.req.query("code") || "",
-    "redirect_uri": DISCORD_REDIRECT_URI,
+    grant_type: "authorization_code",
+    code: c.req.query("code") || "",
+    redirect_uri: DISCORD_REDIRECT_URI,
   })
-  const accessData =
-    await (await fetch(`${DISCORD_API_ENDPOINT}/oauth2/token`, {
+  const accessData = await (
+    await fetch(`${DISCORD_API_ENDPOINT}/oauth2/token`, {
       method: "POST",
       body: accessRequest,
       headers: {
-        "Authorization": "Basic " +
+        Authorization: "Basic " +
           btoa(`${DISCORD_CLIENT_ID}:${DISCORD_CLIENT_SECRET}`),
       },
-    })).json()
-  const userData = await (await fetch(`${DISCORD_API_ENDPOINT}/users/@me`, {
-    headers: { "Authorization": `Bearer ${accessData.access_token}` },
-  })).json()
+    })
+  ).json()
+  const userData = await (
+    await fetch(`${DISCORD_API_ENDPOINT}/users/@me`, {
+      headers: { Authorization: `Bearer ${accessData.access_token}` },
+    })
+  ).json()
   if (userData && userData.id && userData.username) {
     const newUser = {
       userId: userData.id,
